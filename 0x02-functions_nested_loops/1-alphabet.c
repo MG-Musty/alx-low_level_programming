99f0d64687b6cdc4443f9271087f6a8fc271e5b9
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-07-05 13:05:30 -0700

    1. print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,9 @@
 
 
 /**
- * main - prints the alphabet, in lowercase, followed for the new line
+ * print_alphabet - prints the alphabet, in lowercase, followed new line
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter;
 
