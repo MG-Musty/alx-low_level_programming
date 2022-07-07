8569e477cfc51ee8224d79f7632861e991c3b324
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-07-07 13:04:10 -0700

    2. largest number

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,15 +11,15 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	 if ((year % 4 == 0 || year % 400 == 0) || ( year % 400 == 0 && year % 100 == 0))
+	 if ((year % 4 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
-		if (month >= 2 && day >= 60)
-		{
-			day++;
-		}
+	if (month >= 3 && day >= 60)
+	{
+		 day++;
+	}
 
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+	printf("Day of the year: %d\n", day);
+	printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
@@ -28,9 +27,5 @@ void print_remaining_days(int month, int day, int year)
 	{
 	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
-	else
-	{
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 365 - day);
 	}
 }
