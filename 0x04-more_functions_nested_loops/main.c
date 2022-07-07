307dfc50a1d8947e1b71d4885c7f846c3383b9e6
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-07-07 03:43:39 -0700

    0. Isuppercase

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - calls other functions
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	printme();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/printme.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * printme - print me
+ *
+ * Return: nothing
+ */
+void printme(void)
+{
+	_putchar('m');
+	_putchar('e');
+	_putchar('\n');
+}
