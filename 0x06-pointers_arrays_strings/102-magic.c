4fac13760c873f33479213be465d9fa6ddffa5fa
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-07-14 11:00:45 -0700

    10. Magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,8 +2,7 @@
 
 /**
  * main - entry point
- *
- * Return: 0
+ * Return: 0 always success
  */
 int main(void)
 {
@@ -21,7 +20,7 @@ p = &n;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-*(p + 5) = 98;
+*(p = 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
