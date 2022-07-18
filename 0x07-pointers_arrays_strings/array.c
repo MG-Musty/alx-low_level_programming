140483adb7b704168786fbed58ee5c21b7d8cb97
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-07-18 02:38:29 -0700

    0. Begin of memset array

---
+++ 0x07-pointers_arrays_strings/array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * array - print "point"
+ *
+ * Return: nothing
+ */
+void array(void)
+{
+	_putchar('p');
+	_putchar('o');
+	_putchar('i');
+	_putchar('n');
+	_putchar('t');
+	_putchar('\n');
+}
