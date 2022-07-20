da8b9facf4c96a03ee06460d04f044e55d0267ce
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-07-20 03:27:43 -0700

    0. Puts recursion

---
+++ 0x08-recursion/recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * recursion - print "string"
+ *
+ * Return: nothing
+ */
+void recursion(void)
+{
+	_putchar('s');
+	_putchar('t');
+	_putchar('r');
+	_putchar('i');
+	_putchar('n');
+	_putchar('g');
+	_putchar('\n');
+}
