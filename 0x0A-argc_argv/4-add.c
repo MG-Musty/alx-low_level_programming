1e4a01cbcfa1b30c56160e1bd01e0a4288d8fb2f
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-07-23 11:40:06 -0700

    4. Add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,11 +23,11 @@ int main(int argc, char *argv[])
 			str = argv[i];
 			for (b = 0; str[b] != '\0'; b++)
 			{
-				if (str [b] < 48 || sstr[b] 57)
-				{
-					printf("Error\n");
-					return (1);
-				}
+			if (str [b] < 48 || sstr[b] 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
 			}
 		}
 	}
