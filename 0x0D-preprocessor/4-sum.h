2461c08f6992b97dfa7127561f299a92a380f317
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-07-30 14:51:44 -0700

    4. Sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * main - Header file that defines a function-like macro SUM(x, y)
+ * that computes
+ * @x: sum of the number.
+ * @y: the sum of macro
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
