81dc05c84c4a5010c65b88489e756c0ff070f200
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-10-03 09:56:41 -0700

    library files

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+      printf("Hello World!");
+      return (0);
+}
